36dfa61f47050c37ecdf89abd47fcb479daad1a4
qiyuanzh <qiyuan.zhao@gmail.com>
2018-06-05 19:49:28 -0400

    Create 326.py

---
+++ 326.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n<1:
+            return False
+        while not n%3:
+            n/=3
+        return n==1
